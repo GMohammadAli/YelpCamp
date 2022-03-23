3f8a0ba057a3bf7f211731a2e997e25090904de0
GMohammadAli <gulammohammadali@gmail.com>
2022-03-23 22:36:53 +0530

    Section_41

--- seeds/index.js
+++ seeds/index.js
@@ -23,10 +23,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
+          location: `${cities[random1000].city}, ${cities[random1000].state}`,
+          title: `${sample(descriptors)} ${sample(places)}`,
+          image:
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fchain-link-icon-vector-illustration-white-background-image152617478&psig=AOvVaw1Ly4V4Ym5MC3AF2yWEWhrv&ust=1648139017280000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDAtrrS3PYCFQAAAAAdAAAAABAS",
+          description: "Lorem ipsum",
+          price,
+        });
         await camp.save();
     }
 }
